96ad6cd0351c92c3b7e8d6cbc54ba08f58a9c334
g00glen00b <me@g00glen00b.be>
2015-07-05 17:36:19 +0200

    Fixing missing event property

--- app/components/SongForm.jsx
+++ app/components/SongForm.jsx
@@ -4,7 +4,7 @@
     isDisabled:  function() {
       return _.isBlank(this.state.artist) || _.isBlank(this.state.title);
     },
-    handleNewArtist: function() {
+    handleNewArtist: function(event) {
       this.setState(_.extend(this.state, {
         artist: event.target.value
       }));
@@ -18,7 +18,7 @@
       var song = new Song(this.state);
       song.save();
       this.setState(this.getInitialState());
-      
+
       event.preventDefault();
       event.stopPropagation();
     },
